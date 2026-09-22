b4fbe687c6e75b9a1ea4a3b4f38ea62f94789774
ivan <ivan.b@vizbl.us>
2026-09-22 17:30:18 +0700

    Column End alignment

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,27 +1,27 @@
 package com.example.rowcoldemo
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.example.rowcoldemo.ui.theme.RowColDemoTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.rowcoldemo.ui.theme.RowColDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,33 +37,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        Row {
-            Column {
-                TextCell("1")
-                TextCell("2")
-                TextCell("3")
-            }
-            Column {
-                TextCell("4")
-                TextCell("5")
-                TextCell("6")
-            }
-            Column {
-                TextCell("7")
-                TextCell("8")
-            }
-        }
-        Row {
-            TextCell("9")
-            TextCell("10")
-            TextCell("11")
-        }
+    Column(
+        horizontalAlignment = Alignment.End,
+        modifier = modifier.width(250.dp)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
     }
+
 }
+
 @Composable
 fun TextCell(text: String, modifier: Modifier = Modifier) {
     val cellModifier = modifier
@@ -79,6 +65,7 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
         textAlign = TextAlign.Center
     )
 }
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun MainScreenPreview() {
