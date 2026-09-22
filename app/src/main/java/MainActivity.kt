f2ebcc2c24bc0ef01282a9cc448e7137944514e7
ivan <ivan.b@vizbl.us>
2026-09-22 17:41:40 +0700

    Finish

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,23 +1,17 @@
 package com.example.rowcoldemo
-import androidx.compose.foundation.layout.Arrangement
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.height
-
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.border
-//import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -43,10 +37,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.height(300.dp)) {
-        TextCell("1", Modifier.align(Alignment.Top))
-        TextCell("2", Modifier.align(Alignment.CenterVertically))
-        TextCell("3", Modifier.align(Alignment.Bottom))
+    Row {
+        TextCell("1", Modifier.weight(weight = 0.2f, fill = true))
+        TextCell("2", Modifier.weight(weight = 0.4f, fill = true))
+        TextCell("3", Modifier.weight(weight = 0.3f, fill = true))
     }
 }
 
